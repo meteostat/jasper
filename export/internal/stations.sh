6dc39b3d5c65f21bff8e44b4aa6a40ac4b9aa929
clampr <christian.lamprecht@aol.de>
2021-04-04 00:00:15 +0200

    Add regular inventory export

--- export/internal/stations.sh
+++ export/internal/stations.sh
@@ -8,10 +8,13 @@ FTP_USER=`sed -nr "/^\[bulk_ftp\]/ { :l /^user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b
 FTP_PASS=`sed -nr "/^\[bulk_ftp\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
 
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME stations > stations.sql
+mysqldump -u$DB_USER -p$DB_PASS $DB_NAME inventory > inventory.sql
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME stations_inventory > stations_inventory.sql
 
 curl -T stations.sql ftp://$FTP_HOST/internal/ --user $FTP_USER:$FTP_PASS
+curl -T inventory.sql ftp://$FTP_HOST/internal/ --user $FTP_USER:$FTP_PASS
 curl -T stations_inventory.sql ftp://$FTP_HOST/internal/ --user $FTP_USER:$FTP_PASS
 
 rm stations.sql
+rm inventory.sql
 rm stations_inventory.sql
