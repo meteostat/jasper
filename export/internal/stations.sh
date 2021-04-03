b38c4035ad1ae449e506303d0b64f80ea160c8ad
clampr <christian.lamprecht@aol.de>
2021-04-03 23:41:24 +0200

    Correct credentials

--- export/internal/stations.sh
+++ export/internal/stations.sh
@@ -2,7 +2,7 @@ cd ~/.routines
 
 DB_USER=`sed -nr "/^\[database\]/ { :l /^user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
 DB_PASS=`sed -nr "/^\[database\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
-DB_NAME=`sed -nr "/^\[database\]/ { :l /^name‚[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+DB_NAME=`sed -nr "/^\[database\]/ { :l /^name[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
 FTP_HOST=`sed -nr "/^\[bulk_ftp\]/ { :l /^host[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
 FTP_USER=`sed -nr "/^\[bulk_ftp\]/ { :l /^user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
 FTP_PASS=`sed -nr "/^\[bulk_ftp\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
@@ -10,8 +10,8 @@ FTP_PASS=`sed -nr "/^\[bulk_ftp\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; 
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME stations > stations.sql
 mysqldump -u$DB_USER -p$DB_PASS $DB_NAME stations_inventory > stations_inventory.sql
 
-ftp -in ftp://{$FTP_USER}:{$FTP_PASS}@{$FTP_HOST}/internal/stations.sql stations.sql
-ftp -in ftp://{$FTP_USER}:{$FTP_PASS}@{$FTP_HOST}/internal/stations.sql stations_inventory.sql
+ftp ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/internal/stations.sql stations.sql
+ftp ftp://$FTP_USER:$FTP_PASS@$FTP_HOST/internal/stations.sql stations_inventory.sql
 
 rm stations.sql
 rm stations_inventory.sql
