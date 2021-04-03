31ed7c2d857ee90c94bdf0c23e3d476cd1270721
clampr <christian.lamprecht@aol.de>
2021-04-03 23:37:57 +0200

    Use DB config

--- export/internal/stations.sh
+++ export/internal/stations.sh
@@ -1,14 +1,17 @@
 cd ~/.routines
 
-mysqldump -uroot -p`cat /etc/psa/.psa.shadow` meteostat stations > stations.sql
-mysqldump -uroot -p`cat /etc/psa/.psa.shadow` meteostat stations_inventory > stations_inventory.sql
+DB_USER=`sed -nr "/^\[database\]/ { :l /^user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+DB_PASS=`sed -nr "/^\[database\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+DB_NAME=`sed -nr "/^\[database\]/ { :l /^name‚[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+FTP_HOST=`sed -nr "/^\[bulk_ftp\]/ { :l /^host[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+FTP_USER=`sed -nr "/^\[bulk_ftp\]/ { :l /^user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+FTP_PASS=`sed -nr "/^\[bulk_ftp\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
 
-HOST=`sed -nr "/^\[bulk_ftp\]/ { :l /^host[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
-USER=`sed -nr "/^\[bulk_ftp\]/ { :l /^user[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
-PASS=`sed -nr "/^\[bulk_ftp\]/ { :l /^password[ ]*=/ { s/.*=[ ]*//; p; q;}; n; b l;}" ./config.txt`
+mysqldump -u$DB_USER -p$DB_PASS $DB_NAME stations > stations.sql
+mysqldump -u$DB_USER -p$DB_PASS $DB_NAME stations_inventory > stations_inventory.sql
 
-ftp -in -u ftp://{$USER}:{$PASS}@{$HOST}/internal/stations.sql stations.sql
-ftp -in -u ftp://{$USER}:{$PASS}@{$HOST}/internal/stations.sql stations_inventory.sql
+ftp -in ftp://{$USER}:{$PASS}@{$HOST}/internal/stations.sql stations.sql
+ftp -in ftp://{$USER}:{$PASS}@{$HOST}/internal/stations.sql stations_inventory.sql
 
 rm stations.sql
 rm stations_inventory.sql
