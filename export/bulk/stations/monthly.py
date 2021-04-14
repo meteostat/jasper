e0209c33ea43ce220b5d59a0ae758c4c229bbcb5
clampr <christian.lamprecht@aol.de>
2021-04-14 16:53:59 +0200

    Fix monthly export

--- export/bulk/stations/monthly.py
+++ export/bulk/stations/monthly.py
@@ -70,167 +70,183 @@ for station in stations:
 				`station` = :station
 			)
 		UNION ALL
-			(SELECT
+    		(SELECT
 				YEAR(`date`) AS `year`,
                 MONTH(`date`) AS `month`,
-                IF(count(`daily_derived`.`tavg`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`daily_derived`.`tavg`), 1)) AS `tavg`,
-                IF(count(`daily_derived`.`tmin`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`daily_derived`.`tmin`), 1)) AS `tmin`,
-                IF(count(`daily_derived`.`tmax`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`daily_derived`.`tmax`), 1)) AS `tmax`,
-                IF(count(`daily_derived`.`prcp`) < DAY(LAST_DAY(`date`)), NULL, ROUND(SUM(`daily_derived`.`prcp`), 1)) AS `prcp`,
-                IF(count(`daily_derived`.`snow`) < DAY(LAST_DAY(`date`)), NULL, ROUND(MAX(`daily_derived`.`snow`), 1)) AS `snow`,
-                IF(count(`daily_derived`.`wdir`) < DAY(LAST_DAY(`date`)), NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`daily_derived`.`wdir`))), SUM(COS(RADIANS(`daily_derived`.`wdir`)))), 1)) AS `wdir`,
-                IF(count(`daily_derived`.`wspd`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`daily_derived`.`wspd`), 1)) AS `wspd`,
-                IF(count(`daily_derived`.`wpgt`) < DAY(LAST_DAY(`date`)), NULL, ROUND(MAX(`daily_derived`.`wpgt`), 1)) AS `wpgt`,
-                IF(count(`daily_derived`.`pres`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`daily_derived`.`pres`), 1)) AS `pres`,
-                IF(count(`daily_derived`.`tsun`) < DAY(LAST_DAY(`date`)), NULL, ROUND(SUM(`daily_derived`.`tsun`), 1)) AS `tsun`,
+                IF(count(`tavg`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`tavg`), 1)) AS `tavg`,
+                IF(count(`tmin`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`tmin`), 1)) AS `tmin`,
+                IF(count(`tmax`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`tmax`), 1)) AS `tmax`,
+                IF(count(`prcp`) < DAY(LAST_DAY(`date`)), NULL, ROUND(SUM(`prcp`), 1)) AS `prcp`,
+                IF(count(`snow`) < DAY(LAST_DAY(`date`)), NULL, ROUND(MAX(`snow`), 1)) AS `snow`,
+                IF(count(`wdir`) < DAY(LAST_DAY(`date`)), NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`wdir`))), SUM(COS(RADIANS(`wdir`)))), 1)) AS `wdir`,
+                IF(count(`wspd`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`wspd`), 1)) AS `wspd`,
+                IF(count(`wpgt`) < DAY(LAST_DAY(`date`)), NULL, ROUND(MAX(`wpgt`), 1)) AS `wpgt`,
+                IF(count(`pres`) < DAY(LAST_DAY(`date`)), NULL, ROUND(AVG(`pres`), 1)) AS `pres`,
+                IF(count(`tsun`) < DAY(LAST_DAY(`date`)), NULL, ROUND(SUM(`tsun`), 1)) AS `tsun`,
                 "B" AS `priority`
-    		FROM (
-    			(SELECT
-    				`date`,
-    				`tavg`,
-    				`tmin`,
-    				`tmax`,
-    				`prcp`,
-    				`snow`,
-    				NULL AS `wdir`,
-    				`wspd`,
-    				`wpgt`,
-    				`pres`,
-    				`tsun`,
-    				"A" AS `priority`
-    			FROM `daily_national`
-    			WHERE
-    				`station` = :station
-    			)
-    		UNION ALL
-    			(SELECT
-    				`date`,
-    				`tavg`,
-    				`tmin`,
-    				`tmax`,
-    				`prcp`,
-    				`snow`,
-    				`wdir`,
-    				`wspd`,
-    				`wpgt`,
-    				NULL AS `pres`,
-    				`tsun`,
-    				"A" AS `priority`
-    			FROM `daily_ghcn`
-    			WHERE
-    				`station` = :station
-    			)
-    		UNION ALL
-    			(SELECT
-    				DATE(CONVERT_TZ(`hourly_national`.`time`, "UTC", :timezone)) AS `date`,
-    				IF(count(`hourly_national`.`temp`)<24, NULL, ROUND(AVG(`hourly_national`.`temp`),1)) AS `tavg`,
-    				IF(count(`hourly_national`.`temp`)<24, NULL, MIN(`hourly_national`.`temp`)) AS `tmin`,
-    				IF(count(`hourly_national`.`temp`)<24, NULL, MAX(`hourly_national`.`temp`)) AS `tmax`,
-    				IF(count(`hourly_national`.`prcp`)<24, NULL, SUM(`hourly_national`.`prcp`)) AS `prcp`,
-    				NULL AS `snow`,
-    				IF(count(`hourly_national`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_national`.`wdir`))), SUM(COS(RADIANS(`hourly_national`.`wdir`)))), 1)) AS `wdir`,
-    				IF(count(`hourly_national`.`wspd`)<24, NULL, ROUND(AVG(`hourly_national`.`wspd`),1)) AS `wspd`,
-    				NULL AS `wpgt`,
-    				IF(count(`hourly_national`.`pres`)<24, NULL, ROUND(AVG(`hourly_national`.`pres`),1)) AS `pres`,
-    				NULL AS `tsun`,
-    				"B" AS `priority`
-    			FROM `hourly_national`
-    			WHERE
-    				`hourly_national`.`station` = :station
-    			GROUP BY
-    				`station`,
-    				`date`
-    			)
-    		UNION ALL
-    			(SELECT
-    				DATE(CONVERT_TZ(`hourly_isd`.`time`, "UTC", :timezone)) AS `date`,
-    				IF(count(`hourly_isd`.`temp`)<24, NULL, ROUND(AVG(`hourly_isd`.`temp`),1)) AS `tavg`,
-    				IF(count(`hourly_isd`.`temp`)<24, NULL, MIN(`hourly_isd`.`temp`)) AS `tmin`,
-    				IF(count(`hourly_isd`.`temp`)<24, NULL, MAX(`hourly_isd`.`temp`)) AS `tmax`,
-    				IF(count(`hourly_isd`.`prcp`)<24, NULL, SUM(`hourly_isd`.`prcp`)) AS `prcp`,
-    				NULL AS `snow`,
-    				IF(count(`hourly_isd`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_isd`.`wdir`))), SUM(COS(RADIANS(`hourly_isd`.`wdir`)))), 1)) AS `wdir`,
-    				IF(count(`hourly_isd`.`wspd`)<24, NULL, ROUND(AVG(`hourly_isd`.`wspd`),1)) AS `wspd`,
-    				NULL AS `wpgt`,
-    				IF(count(`hourly_isd`.`pres`)<24, NULL, ROUND(AVG(`hourly_isd`.`pres`),1)) AS `pres`,
-    				NULL AS `tsun`,
-    				"B" AS `priority`
-    			FROM `hourly_isd`
-    			WHERE
-    				`hourly_isd`.`station` = :station
-    			GROUP BY
-    				`station`,
-    				`date`
-    			)
-    		UNION ALL
-    			(SELECT
-    				DATE(CONVERT_TZ(`hourly_synop`.`time`, "UTC", :timezone)) AS `date`,
-    				IF(count(`hourly_synop`.`temp`)<24, NULL, ROUND(AVG(`hourly_synop`.`temp`),1)) AS `tavg`,
-    				IF(count(`hourly_synop`.`temp`)<24, NULL, MIN(`hourly_synop`.`temp`)) AS `tmin`,
-    				IF(count(`hourly_synop`.`temp`)<24, NULL, MAX(`hourly_synop`.`temp`)) AS `tmax`,
-    				IF(count(`hourly_synop`.`prcp`)<24, NULL, SUM(`hourly_synop`.`prcp`)) AS `prcp`,
-    				IF(count(`hourly_synop`.`snow`)<24, NULL, MAX(`hourly_synop`.`snow`)) AS `snow`,
-    				IF(count(`hourly_synop`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_synop`.`wdir`))), SUM(COS(RADIANS(`hourly_synop`.`wdir`)))), 1)) AS `wdir`,
-    				IF(count(`hourly_synop`.`wspd`)<24, NULL, ROUND(AVG(`hourly_synop`.`wspd`),1)) AS `wspd`,
-    				IF(count(`hourly_synop`.`wpgt`)<24, NULL, MAX(`wpgt`)) AS `wpgt`,
-    				IF(count(`hourly_synop`.`pres`)<24, NULL, ROUND(AVG(`hourly_synop`.`pres`),1)) AS `pres`,
-    				NULL AS `tsun`,
-    				"C" AS `priority`
-    			FROM `hourly_synop`
-    			WHERE
-    				`hourly_synop`.`station` = :station
-    			GROUP BY
-    				`station`,
-    				`date`
-    			)
-    		UNION ALL
-    			(SELECT
-    				DATE(CONVERT_TZ(`hourly_metar`.`time`, "UTC", :timezone)) AS `date`,
-    				IF(count(`hourly_metar`.`temp`)<24, NULL, ROUND(AVG(`hourly_metar`.`temp`),1)) AS `tavg`,
-    				IF(count(`hourly_metar`.`temp`)<24, NULL, MIN(`hourly_metar`.`temp`)) AS `tmin`,
-    				IF(count(`hourly_metar`.`temp`)<24, NULL, MAX(`hourly_metar`.`temp`)) AS `tmax`,
-    				NULL AS `prcp`,
-    				NULL AS `snow`,
-    				IF(count(`hourly_metar`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_metar`.`wdir`))), SUM(COS(RADIANS(`hourly_metar`.`wdir`)))), 1)) AS `wdir`,
-    				IF(count(`hourly_metar`.`wspd`)<24, NULL, ROUND(AVG(`hourly_metar`.`wspd`),1)) AS `wspd`,
-    				NULL AS `wpgt`,
-    				IF(count(`hourly_metar`.`pres`)<24, NULL, ROUND(AVG(`hourly_metar`.`pres`),1)) AS `pres`,
-    				NULL AS `tsun`,
-    				"D" AS `priority`
-    			FROM `hourly_metar`
-    			WHERE
-    				`hourly_metar`.`station` = :station
-    			GROUP BY
-    				`station`,
-    				`date`
-    			)
-        {f"""
-    		UNION ALL
-    			(SELECT
-    				DATE(CONVERT_TZ(`hourly_model`.`time`, "UTC", :timezone)) AS `date`,
-    				IF(count(`hourly_model`.`temp`)<24, NULL, ROUND(AVG(`hourly_model`.`temp`),1)) AS `tavg`,
-    				IF(count(`hourly_model`.`temp`)<24, NULL, MIN(`hourly_model`.`temp`)) AS `tmin`,
-    				IF(count(`hourly_model`.`temp`)<24, NULL, MAX(`hourly_model`.`temp`)) AS `tmax`,
-    				IF(count(`hourly_model`.`prcp`)<24, NULL, SUM(`hourly_model`.`prcp`)) AS `prcp`,
-    				NULL AS `snow`,
-    				IF(count(`hourly_model`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_model`.`wdir`))), SUM(COS(RADIANS(`hourly_model`.`wdir`)))), 1)) AS `wdir`,
-    				IF(count(`hourly_model`.`wspd`)<24, NULL, ROUND(AVG(`hourly_model`.`wspd`),1)) AS `wspd`,
-    				IF(count(`hourly_model`.`wpgt`)<24, NULL, MAX(`hourly_model`.`wpgt`)) AS `wpgt`,
-    				IF(count(`hourly_model`.`pres`)<24, NULL, ROUND(AVG(`hourly_model`.`pres`),1)) AS `pres`,
-    				NULL AS `tsun`,
-    				"E" AS `priority`
-    			FROM `hourly_model`
-    			WHERE
-    				`hourly_model`.`station` = :station
-    			GROUP BY
-    				`station`,
-    				`date`
-    			)
-        """ if SCOPE == 'full' else ''}
-    		) AS `daily_derived`
-    			GROUP BY
-    				`date`
-	)
+            FROM (
+    			SELECT
+        			`date`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`tavg` ORDER BY `priority`), ",", 1) AS `tavg`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`tmin` ORDER BY `priority`), ",", 1) AS `tmin`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`tmax` ORDER BY `priority`), ",", 1) AS `tmax`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`prcp` ORDER BY `priority`), ",", 1) AS `prcp`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`snow` ORDER BY `priority`), ",", 1) AS `snow`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`wdir` ORDER BY `priority`), ",", 1) AS `wdir`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`wspd` ORDER BY `priority`), ",", 1) AS `wspd`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`wpgt` ORDER BY `priority`), ",", 1) AS `wpgt`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`pres` ORDER BY `priority`), ",", 1) AS `pres`,
+        			SUBSTRING_INDEX(GROUP_CONCAT(`tsun` ORDER BY `priority`), ",", 1) AS `tsun`
+        		FROM (
+        			(SELECT
+        				`date`,
+        				`tavg`,
+        				`tmin`,
+        				`tmax`,
+        				`prcp`,
+        				`snow`,
+        				NULL AS `wdir`,
+        				`wspd`,
+        				`wpgt`,
+        				`pres`,
+        				`tsun`,
+        				"A" AS `priority`
+        			FROM `daily_national`
+        			WHERE
+        				`station` = :station
+        			)
+        		UNION ALL
+        			(SELECT
+        				`date`,
+        				`tavg`,
+        				`tmin`,
+        				`tmax`,
+        				`prcp`,
+        				`snow`,
+        				`wdir`,
+        				`wspd`,
+        				`wpgt`,
+        				NULL AS `pres`,
+        				`tsun`,
+        				"A" AS `priority`
+        			FROM `daily_ghcn`
+        			WHERE
+        				`station` = :station
+        			)
+        		UNION ALL
+        			(SELECT
+        				DATE(CONVERT_TZ(`hourly_national`.`time`, "UTC", :timezone)) AS `date`,
+        				IF(count(`hourly_national`.`temp`)<24, NULL, ROUND(AVG(`hourly_national`.`temp`),1)) AS `tavg`,
+        				IF(count(`hourly_national`.`temp`)<24, NULL, MIN(`hourly_national`.`temp`)) AS `tmin`,
+        				IF(count(`hourly_national`.`temp`)<24, NULL, MAX(`hourly_national`.`temp`)) AS `tmax`,
+        				IF(count(`hourly_national`.`prcp`)<24, NULL, SUM(`hourly_national`.`prcp`)) AS `prcp`,
+        				NULL AS `snow`,
+        				IF(count(`hourly_national`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_national`.`wdir`))), SUM(COS(RADIANS(`hourly_national`.`wdir`)))), 1)) AS `wdir`,
+        				IF(count(`hourly_national`.`wspd`)<24, NULL, ROUND(AVG(`hourly_national`.`wspd`),1)) AS `wspd`,
+        				NULL AS `wpgt`,
+        				IF(count(`hourly_national`.`pres`)<24, NULL, ROUND(AVG(`hourly_national`.`pres`),1)) AS `pres`,
+        				NULL AS `tsun`,
+        				"B" AS `priority`
+        			FROM `hourly_national`
+        			WHERE
+        				`hourly_national`.`station` = :station
+        			GROUP BY
+        				`station`,
+        				`date`
+        			)
+        		UNION ALL
+        			(SELECT
+        				DATE(CONVERT_TZ(`hourly_isd`.`time`, "UTC", :timezone)) AS `date`,
+        				IF(count(`hourly_isd`.`temp`)<24, NULL, ROUND(AVG(`hourly_isd`.`temp`),1)) AS `tavg`,
+        				IF(count(`hourly_isd`.`temp`)<24, NULL, MIN(`hourly_isd`.`temp`)) AS `tmin`,
+        				IF(count(`hourly_isd`.`temp`)<24, NULL, MAX(`hourly_isd`.`temp`)) AS `tmax`,
+        				IF(count(`hourly_isd`.`prcp`)<24, NULL, SUM(`hourly_isd`.`prcp`)) AS `prcp`,
+        				NULL AS `snow`,
+        				IF(count(`hourly_isd`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_isd`.`wdir`))), SUM(COS(RADIANS(`hourly_isd`.`wdir`)))), 1)) AS `wdir`,
+        				IF(count(`hourly_isd`.`wspd`)<24, NULL, ROUND(AVG(`hourly_isd`.`wspd`),1)) AS `wspd`,
+        				NULL AS `wpgt`,
+        				IF(count(`hourly_isd`.`pres`)<24, NULL, ROUND(AVG(`hourly_isd`.`pres`),1)) AS `pres`,
+        				NULL AS `tsun`,
+        				"B" AS `priority`
+        			FROM `hourly_isd`
+        			WHERE
+        				`hourly_isd`.`station` = :station
+        			GROUP BY
+        				`station`,
+        				`date`
+        			)
+        		UNION ALL
+        			(SELECT
+        				DATE(CONVERT_TZ(`hourly_synop`.`time`, "UTC", :timezone)) AS `date`,
+        				IF(count(`hourly_synop`.`temp`)<24, NULL, ROUND(AVG(`hourly_synop`.`temp`),1)) AS `tavg`,
+        				IF(count(`hourly_synop`.`temp`)<24, NULL, MIN(`hourly_synop`.`temp`)) AS `tmin`,
+        				IF(count(`hourly_synop`.`temp`)<24, NULL, MAX(`hourly_synop`.`temp`)) AS `tmax`,
+        				IF(count(`hourly_synop`.`prcp`)<24, NULL, SUM(`hourly_synop`.`prcp`)) AS `prcp`,
+        				IF(count(`hourly_synop`.`snow`)<24, NULL, MAX(`hourly_synop`.`snow`)) AS `snow`,
+        				IF(count(`hourly_synop`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_synop`.`wdir`))), SUM(COS(RADIANS(`hourly_synop`.`wdir`)))), 1)) AS `wdir`,
+        				IF(count(`hourly_synop`.`wspd`)<24, NULL, ROUND(AVG(`hourly_synop`.`wspd`),1)) AS `wspd`,
+        				IF(count(`hourly_synop`.`wpgt`)<24, NULL, MAX(`wpgt`)) AS `wpgt`,
+        				IF(count(`hourly_synop`.`pres`)<24, NULL, ROUND(AVG(`hourly_synop`.`pres`),1)) AS `pres`,
+        				NULL AS `tsun`,
+        				"C" AS `priority`
+        			FROM `hourly_synop`
+        			WHERE
+        				`hourly_synop`.`station` = :station
+        			GROUP BY
+        				`station`,
+        				`date`
+        			)
+        		UNION ALL
+        			(SELECT
+        				DATE(CONVERT_TZ(`hourly_metar`.`time`, "UTC", :timezone)) AS `date`,
+        				IF(count(`hourly_metar`.`temp`)<24, NULL, ROUND(AVG(`hourly_metar`.`temp`),1)) AS `tavg`,
+        				IF(count(`hourly_metar`.`temp`)<24, NULL, MIN(`hourly_metar`.`temp`)) AS `tmin`,
+        				IF(count(`hourly_metar`.`temp`)<24, NULL, MAX(`hourly_metar`.`temp`)) AS `tmax`,
+        				NULL AS `prcp`,
+        				NULL AS `snow`,
+        				IF(count(`hourly_metar`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_metar`.`wdir`))), SUM(COS(RADIANS(`hourly_metar`.`wdir`)))), 1)) AS `wdir`,
+        				IF(count(`hourly_metar`.`wspd`)<24, NULL, ROUND(AVG(`hourly_metar`.`wspd`),1)) AS `wspd`,
+        				NULL AS `wpgt`,
+        				IF(count(`hourly_metar`.`pres`)<24, NULL, ROUND(AVG(`hourly_metar`.`pres`),1)) AS `pres`,
+        				NULL AS `tsun`,
+        				"D" AS `priority`
+        			FROM `hourly_metar`
+        			WHERE
+        				`hourly_metar`.`station` = :station
+        			GROUP BY
+        				`station`,
+        				`date`
+        			)
+            {f"""
+        		UNION ALL
+        			(SELECT
+        				DATE(CONVERT_TZ(`hourly_model`.`time`, "UTC", :timezone)) AS `date`,
+        				IF(count(`hourly_model`.`temp`)<24, NULL, ROUND(AVG(`hourly_model`.`temp`),1)) AS `tavg`,
+        				IF(count(`hourly_model`.`temp`)<24, NULL, MIN(`hourly_model`.`temp`)) AS `tmin`,
+        				IF(count(`hourly_model`.`temp`)<24, NULL, MAX(`hourly_model`.`temp`)) AS `tmax`,
+        				IF(count(`hourly_model`.`prcp`)<24, NULL, SUM(`hourly_model`.`prcp`)) AS `prcp`,
+        				NULL AS `snow`,
+        				IF(count(`hourly_model`.`wdir`)<24, NULL, ROUND(DEGAVG(SUM(SIN(RADIANS(`hourly_model`.`wdir`))), SUM(COS(RADIANS(`hourly_model`.`wdir`)))), 1)) AS `wdir`,
+        				IF(count(`hourly_model`.`wspd`)<24, NULL, ROUND(AVG(`hourly_model`.`wspd`),1)) AS `wspd`,
+        				IF(count(`hourly_model`.`wpgt`)<24, NULL, MAX(`hourly_model`.`wpgt`)) AS `wpgt`,
+        				IF(count(`hourly_model`.`pres`)<24, NULL, ROUND(AVG(`hourly_model`.`pres`),1)) AS `pres`,
+        				NULL AS `tsun`,
+        				"E" AS `priority`
+        			FROM `hourly_model`
+        			WHERE
+        				`hourly_model`.`station` = :station
+        			GROUP BY
+        				`station`,
+        				`date`
+        			)
+            """ if SCOPE == 'full' else ''}
+        		) AS `daily_derived`
+        		GROUP BY
+        		      `date`
+    	) AS `daily_aggregated`
+        GROUP BY
+            EXTRACT(YEAR_MONTH FROM `date`)
+    )
 ) AS `monthly_derived`
 	WHERE
 		`tavg` IS NOT NULL
