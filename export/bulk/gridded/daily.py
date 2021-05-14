4d9b97a7ed75ac7f1ebb3f49a3cdc81731fed08b
clampr <christian.lamprecht@aol.de>
2021-05-14 13:49:37 +0200

    Grid Export Query

--- export/bulk/gridded/daily.py
+++ export/bulk/gridded/daily.py
@@ -15,13 +15,13 @@ import verde as vd
 from routines import Routine
 
 # Configuration
-date: datetime = datetime.date.today() - datetime.timedelta(days=7)
+delta: int = int(argv[1])
+date: datetime = datetime.date.today() - datetime.timedelta(days=delta)
 parameters: list = [
 	'tavg',
 	'tmin',
 	'tmax',
 	'prcp',
-	'wdir',
 	'wspd',
 	'wpgt',
 	'pres'
@@ -42,25 +42,24 @@ raw = pd.read_sql(f'''
 		SUBSTRING_INDEX(GROUP_CONCAT(`tmin` ORDER BY `priority`), ",", 1) AS `tmin`,
 		SUBSTRING_INDEX(GROUP_CONCAT(`tmax` ORDER BY `priority`), ",", 1) AS `tmax`,
 		SUBSTRING_INDEX(GROUP_CONCAT(`prcp` ORDER BY `priority`), ",", 1) AS `prcp`,
-		SUBSTRING_INDEX(GROUP_CONCAT(`wdir` ORDER BY `priority`), ",", 1) AS `wdir`,
 		SUBSTRING_INDEX(GROUP_CONCAT(`wspd` ORDER BY `priority`), ",", 1) AS `wspd`,
 		SUBSTRING_INDEX(GROUP_CONCAT(`wpgt` ORDER BY `priority`), ",", 1) AS `wpgt`,
 		SUBSTRING_INDEX(GROUP_CONCAT(`pres` ORDER BY `priority`), ",", 1) AS `pres`
 	FROM (
+{f"""
 		(SELECT
 			`station`,
 			`tavg`,
 			`tmin`,
 			`tmax`,
 			`prcp`,
-			NULL AS `wdir`,
 			`wspd`,
 			`wpgt`,
 			`pres`,
 			"A" AS `priority`
 		FROM `daily_national`
 		WHERE
-			`date` = "{date.strftime('%y-%m-%d')}"
+			`date` = '{date.strftime('%Y-%m-%d')}'
 		)
 	UNION ALL
 		(SELECT
@@ -69,14 +68,139 @@ raw = pd.read_sql(f'''
 			`tmin`,
 			`tmax`,
 			`prcp`,
-			`wdir`,
 			`wspd`,
 			`wpgt`,
 			NULL AS `pres`,
 			"A" AS `priority`
 		FROM `daily_ghcn`
 		WHERE
-			`date` = "{date.strftime('%y-%m-%d')}"
+			`date` = '{date.strftime('%Y-%m-%d')}'
+		)
+	UNION ALL
+		(SELECT
+			`hourly_national`.`station` AS `station`,
+			IF(count(`hourly_national`.`temp`)<24, NULL, ROUND(AVG(`hourly_national`.`temp`), 1)) AS `tavg`,
+			IF(count(`hourly_national`.`temp`)<24, NULL, MIN(`hourly_national`.`temp`)) AS `tmin`,
+			IF(count(`hourly_national`.`temp`)<24, NULL, MAX(`hourly_national`.`temp`)) AS `tmax`,
+			IF(count(`hourly_national`.`prcp`)<24, NULL, SUM(`hourly_national`.`prcp`)) AS `prcp`,
+			IF(count(`hourly_national`.`wspd`)<24, NULL, ROUND(AVG(`hourly_national`.`wspd`), 1)) AS `wspd`,
+			NULL AS `wpgt`,
+			IF(count(`hourly_national`.`pres`)<24, NULL, ROUND(AVG(`hourly_national`.`pres`), 1)) AS `pres`,
+			"B" AS `priority`
+		FROM `hourly_national`
+		INNER JOIN `stations`
+		ON
+			`hourly_national`.`station` = `stations`.`id` AND
+			`hourly_national`.`time` BETWEEN
+				DATE_SUB('{date.strftime('%Y-%m-%d')} 00:00:00', INTERVAL 12 HOUR) AND
+				DATE_ADD('{date.strftime('%Y-%m-%d')} 23:59:59', INTERVAL 12 HOUR)
+		WHERE
+			`hourly_national`.`time` BETWEEN
+				CONVERT_TZ('{date.strftime('%Y-%m-%d')} 00:00:00', `stations`.`tz`, "UTC") AND
+				CONVERT_TZ('{date.strftime('%Y-%m-%d')} 23:59:59', `stations`.`tz`, "UTC")
+		GROUP BY
+			`station`
+		)
+	UNION ALL
+		(SELECT
+			`hourly_isd`.`station` AS `station`,
+			IF(count(`hourly_isd`.`temp`)<24, NULL, ROUND(AVG(`hourly_isd`.`temp`),1)) AS `tavg`,
+			IF(count(`hourly_isd`.`temp`)<24, NULL, MIN(`hourly_isd`.`temp`)) AS `tmin`,
+			IF(count(`hourly_isd`.`temp`)<24, NULL, MAX(`hourly_isd`.`temp`)) AS `tmax`,
+			IF(count(`hourly_isd`.`prcp`)<24, NULL, SUM(`hourly_isd`.`prcp`)) AS `prcp`,
+			IF(count(`hourly_isd`.`wspd`)<24, NULL, ROUND(AVG(`hourly_isd`.`wspd`),1)) AS `wspd`,
+			NULL AS `wpgt`,
+			IF(count(`hourly_isd`.`pres`)<24, NULL, ROUND(AVG(`hourly_isd`.`pres`),1)) AS `pres`,
+			"B" AS `priority`
+		FROM `hourly_isd`
+		INNER JOIN `stations`
+		ON
+			`hourly_isd`.`station` = `stations`.`id` AND
+			`hourly_isd`.`time` BETWEEN
+				DATE_SUB('{date.strftime('%Y-%m-%d')} 00:00:00', INTERVAL 12 HOUR) AND
+				DATE_ADD('{date.strftime('%Y-%m-%d')} 23:59:59', INTERVAL 12 HOUR)
+		WHERE
+			`hourly_isd`.`time` BETWEEN
+				CONVERT_TZ('{date.strftime('%Y-%m-%d')} 00:00:00', `stations`.`tz`, "UTC") AND
+				CONVERT_TZ('{date.strftime('%Y-%m-%d')} 23:59:59', `stations`.`tz`, "UTC")
+		GROUP BY
+			`station`
+		)
+	UNION ALL
+""" if delta > 7 else ''}
+		(SELECT
+			`hourly_synop`.`station` AS `station`,
+			IF(count(`hourly_synop`.`temp`)<24, NULL, ROUND(AVG(`hourly_synop`.`temp`),1)) AS `tavg`,
+			IF(count(`hourly_synop`.`temp`)<24, NULL, MIN(`hourly_synop`.`temp`)) AS `tmin`,
+			IF(count(`hourly_synop`.`temp`)<24, NULL, MAX(`hourly_synop`.`temp`)) AS `tmax`,
+			IF(count(`hourly_synop`.`prcp`)<24, NULL, SUM(`hourly_synop`.`prcp`)) AS `prcp`,
+			IF(count(`hourly_synop`.`wspd`)<24, NULL, ROUND(AVG(`hourly_synop`.`wspd`),1)) AS `wspd`,
+			IF(count(`hourly_synop`.`wpgt`)<24, NULL, MAX(`wpgt`)) AS `wpgt`,
+			IF(count(`hourly_synop`.`pres`)<24, NULL, ROUND(AVG(`hourly_synop`.`pres`),1)) AS `pres`,
+			"C" AS `priority`
+		FROM `hourly_synop`
+		INNER JOIN `stations`
+		ON
+			`hourly_synop`.`station` = `stations`.`id` AND
+			`hourly_synop`.`time` BETWEEN
+				DATE_SUB("{date.strftime('%Y-%m-%d')} 00:00:00", INTERVAL 12 HOUR) AND
+				DATE_ADD("{date.strftime('%Y-%m-%d')} 23:59:59", INTERVAL 12 HOUR)
+		WHERE
+			`hourly_synop`.`time` BETWEEN
+				CONVERT_TZ("{date.strftime('%Y-%m-%d')} 00:00:00", `stations`.`tz`, "UTC") AND
+				CONVERT_TZ("{date.strftime('%Y-%m-%d')} 23:59:59", `stations`.`tz`, "UTC")
+		GROUP BY
+			`station`
+		)
+	UNION ALL
+		(SELECT
+			`hourly_metar`.`station` AS `station`,
+			IF(count(`hourly_metar`.`temp`)<24, NULL, ROUND(AVG(`hourly_metar`.`temp`),1)) AS `tavg`,
+			IF(count(`hourly_metar`.`temp`)<24, NULL, MIN(`hourly_metar`.`temp`)) AS `tmin`,
+			IF(count(`hourly_metar`.`temp`)<24, NULL, MAX(`hourly_metar`.`temp`)) AS `tmax`,
+			NULL AS `prcp`,
+			IF(count(`hourly_metar`.`wspd`)<24, NULL, ROUND(AVG(`hourly_metar`.`wspd`),1)) AS `wspd`,
+			NULL AS `wpgt`,
+			IF(count(`hourly_metar`.`pres`)<24, NULL, ROUND(AVG(`hourly_metar`.`pres`),1)) AS `pres`,
+			"D" AS `priority`
+		FROM `hourly_metar`
+		INNER JOIN `stations`
+		ON
+			`hourly_metar`.`station` = `stations`.`id` AND
+			`hourly_metar`.`time` BETWEEN
+				DATE_SUB("{date.strftime('%Y-%m-%d')} 00:00:00", INTERVAL 12 HOUR) AND
+				DATE_ADD("{date.strftime('%Y-%m-%d')} 23:59:59", INTERVAL 12 HOUR)
+		WHERE
+			`hourly_metar`.`time` BETWEEN
+				CONVERT_TZ("{date.strftime('%Y-%m-%d')} 00:00:00", `stations`.`tz`, "UTC") AND
+				CONVERT_TZ("{date.strftime('%Y-%m-%d')} 23:59:59", `stations`.`tz`, "UTC")
+		GROUP BY
+			`station`
+		)
+	UNION ALL
+		(SELECT
+			`hourly_model`.`station` AS `station`,
+			IF(count(`hourly_model`.`temp`)<24, NULL, ROUND(AVG(`hourly_model`.`temp`),1)) AS `tavg`,
+			IF(count(`hourly_model`.`temp`)<24, NULL, MIN(`hourly_model`.`temp`)) AS `tmin`,
+			IF(count(`hourly_model`.`temp`)<24, NULL, MAX(`hourly_model`.`temp`)) AS `tmax`,
+			IF(count(`hourly_model`.`prcp`)<24, NULL, SUM(`hourly_model`.`prcp`)) AS `prcp`,
+			IF(count(`hourly_model`.`wspd`)<24, NULL, ROUND(AVG(`hourly_model`.`wspd`),1)) AS `wspd`,
+			IF(count(`hourly_model`.`wpgt`)<24, NULL, MAX(`hourly_model`.`wpgt`)) AS `wpgt`,
+			IF(count(`hourly_model`.`pres`)<24, NULL, ROUND(AVG(`hourly_model`.`pres`),1)) AS `pres`,
+			"E" AS `priority`
+		FROM `hourly_model`
+		INNER JOIN `stations`
+		ON
+			`hourly_model`.`station` = `stations`.`id` AND
+			`hourly_model`.`time` BETWEEN
+				DATE_SUB("{date.strftime('%Y-%m-%d')} 00:00:00", INTERVAL 12 HOUR) AND
+				DATE_ADD("{date.strftime('%Y-%m-%d')} 23:59:59", INTERVAL 12 HOUR)
+		WHERE
+			`hourly_model`.`time` BETWEEN
+				CONVERT_TZ("{date.strftime('%Y-%m-%d')} 00:00:00", `stations`.`tz`, "UTC") AND
+				CONVERT_TZ("{date.strftime('%Y-%m-%d')} 23:59:59", `stations`.`tz`, "UTC")
+		GROUP BY
+			`station`
 		)
 	) AS `daily_derived`
 	INNER JOIN `stations`
