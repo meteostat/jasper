c5fa4b4870ffed037afd94e6b103fcf94d4e06bb
clampr <christian.lamprecht@aol.de>
2021-06-11 01:57:15 +0200

    Add try...except 2

--- export/bulk/stations/normals.py
+++ export/bulk/stations/normals.py
@@ -30,109 +30,119 @@ stations = task.get_stations(f'''
         )
 ''', STATIONS_PER_CYCLE)
 
-stations = [['10637']]
-
 # Export data for each weather station
 for station in stations:
 
-    # Full DataFrame
-    data: pd.DataFrame = pd.DataFrame()
-
-    # Get centuries
-    decades = []
-    loop_year = 1990
-    current_year = datetime.now().year
-    while loop_year < current_year:
-        decades.append(loop_year)
-        loop_year += 10
-
-    # Collect normals from Monthly interface
-    for year in decades:
-
-        # Start & end year
-        start = datetime(year-29, 1, 1)
-        end = datetime(year, 12, 31)
-        # Get data
-        df = Monthly(station[0], start, end)
-        # Get coverage data
-        coverage = {}
-        for parameter in df._columns[2:]:
-            coverage[parameter] = df.coverage(parameter)
-        # Fetch DataFrame
-        df = df.fetch()
-        # Drop certain columns
-        df = df.drop(['snow', 'wdir', 'wpgt'], axis=1)
-        # Aggregate monthly
-        df = df.groupby(df.index.month).agg('mean')
-        df = df.round(1)
-        # Refactor index
-        df.reset_index(inplace=True)
-        df = df.reset_index()
-        df['start'] = year - 29
-        df['end'] = year
-        df.set_index(['start', 'end', 'time'], inplace=True)
-        # Remove uncertain data
-        for parameter in coverage:
-            if parameter in df.columns and coverage[parameter] < 0.6:
-                df[parameter] = np.NaN
-
-        data = data.append(df)
-
-    # Get normals from DB
-    data_db = pd.read_sql(f'''
-		SET STATEMENT
-			max_statement_time=60
-		FOR
-		SELECT
-            `start`,
-            `end`,
-            `month` AS `time`,
-            `tavg`,
-            `tmin`,
-            `tmax`,
-            `prcp`,
-            `pres`,
-            `tsun`
-        FROM
-            `normals_global`
-        WHERE
-            `station` = "{station[0]}"
-        ORDER BY
-            `start`,
-            `end`,
-            `month`
-    ''',
-    task.db,
-    index_col=['start', 'end', 'time'])
-
-    # Merge data
-    if data_db.index.size > 0:
-        data = pd.concat([data, data_db])
-        data = data.groupby([
-            data.index.get_level_values('start'),
-            data.index.get_level_values('end'),
-            data.index.get_level_values('time')]
-        ).agg('last')
-
-    # Convert to list
     try:
-        data = data.drop('index', axis=1)
+
+        # Full DataFrame
+        data: pd.DataFrame = pd.DataFrame()
+
+        # Get centuries
+        decades = []
+        loop_year = 1990
+        current_year = datetime.now().year
+        while loop_year < current_year:
+            decades.append(loop_year)
+            loop_year += 10
+
+        # Collect normals from Monthly interface
+        for year in decades:
+
+            try:
+
+                # Start & end year
+                start = datetime(year-29, 1, 1)
+                end = datetime(year, 12, 31)
+                # Get data
+                df = Monthly(station[0], start, end)
+                # Get coverage data
+                coverage = {}
+                for parameter in df._columns[2:]:
+                    coverage[parameter] = df.coverage(parameter)
+                # Fetch DataFrame
+                df = df.fetch()
+                # Drop certain columns
+                df = df.drop(['snow', 'wdir', 'wpgt'], axis=1)
+                # Aggregate monthly
+                df = df.groupby(df.index.month).agg('mean')
+                df = df.round(1)
+                # Refactor index
+                df.reset_index(inplace=True)
+                df = df.reset_index()
+                df['start'] = year - 29
+                df['end'] = year
+                df.set_index(['start', 'end', 'time'], inplace=True)
+                # Remove uncertain data
+                for parameter in coverage:
+                    if parameter in df.columns and coverage[parameter] < 0.6:
+                        df[parameter] = np.NaN
+
+                data = data.append(df)
+
+            except BaseException:
+
+                pass
+
+        # Get normals from DB
+        data_db = pd.read_sql(f'''
+    		SET STATEMENT
+    			max_statement_time=60
+    		FOR
+    		SELECT
+                `start`,
+                `end`,
+                `month` AS `time`,
+                `tavg`,
+                `tmin`,
+                `tmax`,
+                `prcp`,
+                `pres`,
+                `tsun`
+            FROM
+                `normals_global`
+            WHERE
+                `station` = "{station[0]}"
+            ORDER BY
+                `start`,
+                `end`,
+                `month`
+        ''',
+        task.db,
+        index_col=['start', 'end', 'time'])
+
+        # Merge data
+        if data_db.index.size > 0:
+            data = pd.concat([data, data_db])
+            data = data.groupby([
+                data.index.get_level_values('start'),
+                data.index.get_level_values('end'),
+                data.index.get_level_values('time')]
+            ).agg('last')
+
+        # Convert to list
+        try:
+            data = data.drop('index', axis=1)
+        except BaseException:
+            pass
+        data = data.reset_index().to_dict('records')
+        data = list(map(lambda d: d.values(), data))
+
+        if len(data) > 0:
+
+            file = BytesIO()
+
+            with GzipFile(fileobj=file, mode='w') as gz:
+                output = StringIO()
+                writer = csv.writer(output, delimiter=',')
+                writer.writerows(data)
+                gz.write(output.getvalue().encode())
+                gz.close()
+                file.seek(0)
+
+            task.bulk_ftp.cwd(f'/normals')
+            task.bulk_ftp.storbinary(f'STOR {station[0]}.csv.gz', file)
+
     except BaseException:
+
         pass
-    data = data.reset_index().to_dict('records')
-    data = list(map(lambda d: d.values(), data))
-
-    if len(data) > 0:
-
-        file = BytesIO()
-
-        with GzipFile(fileobj=file, mode='w') as gz:
-            output = StringIO()
-            writer = csv.writer(output, delimiter=',')
-            writer.writerows(data)
-            gz.write(output.getvalue().encode())
-            gz.close()
-            file.seek(0)
-
-        task.bulk_ftp.cwd(f'/normals')
-        task.bulk_ftp.storbinary(f'STOR {station[0]}.csv.gz', file)
