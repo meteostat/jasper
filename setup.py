546fa33eed2cd0f3e70a784281d7a766e5685a17
clampr <christian.lamprecht@aol.de>
2021-06-16 12:39:05 +0200

    Activate normals inventory

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'numba',
         'pykdtree',
         'verde==1.6.1',
-        'meteostat==1.3.5'],
+        'meteostat==1.4.0'],
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
