df5902fd2f936c7cc349052eb1329100126403d5
clampr <christian.lamprecht@aol.de>
2021-07-12 14:28:14 +0200

    Wrap validations in try blocks

--- routines/validation.py
+++ routines/validation.py
@@ -3,109 +3,182 @@ from numpy import isnan
 
 def temp(value):
 
-    if value is None or isnan(value) or value < -100 or value > 65:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < -100 or value > 65:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def rhum(value):
 
-    if value is None or isnan(value) or value < 0 or value > 100:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 100:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def prcp_hourly(value):
 
-    if value is None or isnan(value) or value < 0 or value > 350:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 350:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def prcp_daily(value):
 
-    if value is None or isnan(value) or value < 0 or value > 2000:
+    try:
+
+        if value is None or isnan(value) or value < 0 or value > 2000:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
         return None
-    else:
-        return value
 
 def prcp_monthly(value):
 
-    if value is None or isnan(value) or value < 0 or value > 10000:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 10000:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def wspd(value):
 
-    if value is None or isnan(value) or value < 0 or value > 250:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 250:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def wpgt(value):
 
-    if value is None or isnan(value) or value < 0 or value > 500:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 500:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def wdir(value):
 
-    if value is None or isnan(value) or value < 0 or value > 360:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 360:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def pres(value):
 
-    if value is None or isnan(value) or value < 850 or value > 1090:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 850 or value > 1090:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def snow(value):
 
-    if value is None or isnan(value) or value < 0 or value > 11000:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 11000:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def tsun_hourly(value):
 
-    if value is None or isnan(value) or value < 0 or value > 60:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 60:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def tsun_daily(value):
 
-    if value is None or isnan(value) or value < 0 or value > 1440:
+    try:
+
+        if value is None or isnan(value) or value < 0 or value > 1440:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
         return None
-    else:
-        return value
 
 def tsun_monthly(value):
 
-    if value is None or isnan(value) or value < 0 or value > 44640:
-        return None
-    else:
-        return value
+    try:
 
+        if value is None or isnan(value) or value < 0 or value > 44640:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
+        return None
 
 def coco(value):
 
-    if value is None or isnan(value) or value < 0 or value > 27:
+    try:
+
+        if value is None or isnan(value) or value < 0 or value > 27:
+            return None
+        else:
+            return value
+
+    except BaseException:
+
         return None
-    else:
-        return value
